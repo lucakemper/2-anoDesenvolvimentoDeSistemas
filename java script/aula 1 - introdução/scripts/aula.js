636873bcd0fdf9d162090a5c1875a53065565048
lucakemper <luca.kemperdrive@gmail.com>
2026-04-16 16:06:06 -0300

    Java.
    primeira aula java

---
+++ java script/aula 1 - introdução/scripts/aula.js
@@ -0,0 +1,31 @@
+// RESPOSTAS CONSOLE
+console.log("Olá, mundo!");
+
+// CAIXA DE DIALOGO
+//alert("chamaaaaaa!!!!!!");
+//prompt("Qual é o seu nome?");
+//confirm("voce faria um pix para seu colega?");
+
+// acessa o documento, busca o elemento com o id selecionado
+//inner = dentro
+document.getElementById("resposta").innerText = "<h1>que legal</h1>";
+document.getElementById("resposta").innerHTML = "<h1>que legal</h1>";
+
+// string - caractere ( "luca" )
+var nome = "Luca"; 
+
+// boolean - logico ('verdadeiro'- true, 'falso' - false )
+var ligado = true;
+//number - número ( 80 )
+// *obs para numeros quebrados - float()
+// *obs para numeros inteiros - int()
+var idade = 23
+var dinheiro = 108.50 ;
+
+//array - lista , vetor
+var listaCompras = []
+console.log(typeof(nome));
+console.log(typeof(ligado));
+console.log(typeof(idade)) ;
+consoele.log (typeof(dinheiro))
+console.log (typeof(listaCompras))
---
+++ java script/aula 1 - introdução/scripts/usuario.js
@@ -0,0 +1,4 @@
+var nome = prompt ("Qual é seu nome ?")
+
+//document.getElementById("resposta").innerHTML = <h1> + nome + </h1>
+document.getElementById("resposta").innerHTML = '<h1> Ola seja bem vindo ${nome}  </h1>';
