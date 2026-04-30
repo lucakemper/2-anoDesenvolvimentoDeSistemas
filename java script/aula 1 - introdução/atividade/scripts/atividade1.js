2874717efa11a2f7aeac679794115eae931c8ea3
lucakemper <luca.kemperdrive@gmail.com>
2026-04-30 14:20:21 -0300

    .
    .

--- java script/aula 1 - introdução/atividade/scripts/atividade1.js
+++
@@ -1,6 +0,0 @@
-var quantidade = prompt ("quantas pessoas na mesa?");
-var valor = prompt ("valor da conta :")
-
-var valorTotal = valor / quantidade;
-
-document.getElementById("valorTotal").innerHTML = "<h1>" + valorTotal + "</h1>";
